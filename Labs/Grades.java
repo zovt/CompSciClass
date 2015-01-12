12360d5fed4cff8c6d385aff94076ac521a53f61
zovt <zovt@gmail.com>
2015-01-12 14:28:21 -0500

    Added a whole lotta labs

---
+++ Labs/Grades.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Grades {
+    public static void main (String[] args) {
+	double grade; // a student's grade
+	double sumOfGrades; // a running total of the student grades
+	int numStudents; // a count of the students
+	int numPass; // a count of the number who will pass
+	int numFail; // a count of the number who will fail
+    }
+}
