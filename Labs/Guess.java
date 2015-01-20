9bf2e68ea6bf048adf14f39cac9d1c596c2fe893
ragesalmon <ragesalmon@gmail.com>
2015-01-19 21:16:06 -0500

    Fixed lab

--- Labs/Guess.java
+++ Labs/Guess.java
@@ -5,10 +5,16 @@ public class Guess {
     public static void main (String[] args) {
 	int numToGuess; // Number the user tries to guess
 	int guess;	// The user's guess
+	int totalGuesses, lowGuesses, highGuesses;
 
 	Scanner scan = new Scanner(System.in);
 	Random gen = new Random();
 
+	// initialize guess nums to 0
+	totalGuesses = 0;
+	lowGuesses = 0;
+	highGuesses = 0;
+	
 	// randomly generate the number to guess
 	numToGuess = gen.nextInt(10) + 1;
 
@@ -18,15 +24,28 @@ public class Guess {
 	    // print message saying guess is wrong
 	    System.out.print("Enter a guess: ");
 	    guess = scan.nextInt();
+	    totalGuesses++;
 
 	    if (guess == numToGuess)
 		break;
-	    System.out.println("Nope!");
+	    else if (guess < numToGuess) {
+		System.out.println("Too low!");
+		lowGuesses++;
+	    }
+	    else {
+		System.out.println("Too high!");
+		highGuesses++;
+	    }
 	}
 
 	System.out.println("You win!!!");
+	System.out.println("Low Guesses: " + lowGuesses);
+	System.out.println("High Guesses: " + highGuesses);
+	System.out.println("Total Guesses: " + totalGuesses);
     }
 }
 	
 
 	     
+    
+    
